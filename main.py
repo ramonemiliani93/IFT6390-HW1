c81c419584f861f91817488c4f22a30c6d30e606
Ramon Daniel Emiliani Mejia <ramon@ramons-mbp.sf.umontreal.ca>
2018-09-25 12:55:34 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class DGPDE:
+    """
+    Class implementation for a diagonal Gaussian parametric density estimator that works for d dimensions.
+    """
+    # TODO
+
+
+class PDE:
+    """
+    Class impementation for a Parzen density estimator with an isotropic Gaussian kernel that works for d dimensions.
+    """
+    # TODO
+
+
+if __name__ == "__main__":
+    pass
